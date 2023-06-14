c719fe48ab9711fb832320d38bbdd1e16f875ef2
Vicente Muñoz <107875064+DomingoVicente@users.noreply.github.com>
2023-06-14 12:10:04 -0400

    Create script.sql

---
+++ parte A/script.sql
@@ -0,0 +1,51 @@
+-- Creación de las tablas
+CREATE TABLE 100mediosPrensa (
+  nombre_medio VARCHAR(100) PRIMARY KEY,
+  ubicacion VARCHAR(100),
+  cobertura VARCHAR(100)
+);
+
+CREATE TABLE fundador (
+  nombre_fundadores VARCHAR(100) PRIMARY KEY,
+  añoFundado INTEGER
+);
+
+CREATE TABLE redesSociales (
+  redSocial VARCHAR(100) PRIMARY KEY,
+  seguidores INTEGER,
+  actualizacionSeguidores DATE,
+  nombre_medio VARCHAR(100),
+  FOREIGN KEY (nombre_medio) REFERENCES 100mediosPrensa(nombre_medio)
+);
+
+CREATE TABLE sitioWeb (
+  nombre_sitioWeb VARCHAR(100) PRIMARY KEY,
+  nombre_medio VARCHAR(100),
+  FOREIGN KEY (nombre_medio) REFERENCES 100mediosPrensa(nombre_medio)
+);
+
+CREATE TABLE categoria (
+  nombre_categoria VARCHAR(100) PRIMARY KEY,
+  URL VARCHAR(100),
+  XPATH VARCHAR(100),
+  nombre_sitioWeb VARCHAR(100),
+  FOREIGN KEY (nombre_sitioWeb) REFERENCES sitioWeb(nombre_sitioWeb)
+);
+
+CREATE TABLE noticia (
+  URL_noticia VARCHAR(100) PRIMARY KEY,
+  XPATH_titulo VARCHAR(100),
+  XPATH_fecha VARCHAR(100),
+  XPATH_contenido VARCHAR(100),
+  nombre_sitioWeb VARCHAR(100),
+  FOREIGN KEY (nombre_sitioWeb) REFERENCES sitioWeb(nombre_sitioWeb)
+);
+
+-- Creación de la tabla de relación entre fundadores y medios de prensa
+CREATE TABLE crea (
+  nombre_fundadores VARCHAR(100),
+  nombre_medio VARCHAR(100),
+  PRIMARY KEY (nombre_fundadores, nombre_medio),
+  FOREIGN KEY (nombre_fundadores) REFERENCES fundador(nombre_fundadores),
+  FOREIGN KEY (nombre_medio) REFERENCES 100mediosPrensa(nombre_medio)
+);
